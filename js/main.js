87719c07b93f529e0f49d9fdfe4d3c34c42b63b7
rsu <rsu@taglineinfotech.com>
2024-12-13 12:10:36 +0530

    responsive hero section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ $(window).scroll(function () {
     $(".header-wrapper").removeClass("position-fixed w-100");
   }
 });
+
+$(".company-slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1.5,
+    },
+    425: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    900: {
+      items: 4,
+    },
+    1000: {
+      items: 6,
+    },
+  },
+});
